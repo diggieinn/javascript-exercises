5bca92350a89ff5c7b9823c4c2ac8d09eb5bfcc0
Bazan <bazandiogo@gmail.com>
2026-01-01 19:36:59 +0000

    java exercises completed

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let index = parseInt(n)
+    if (index <0){
+        return "OOPS";
+    }
+    if (index == 1 ){
+        return 1;
+    }
+    if (index == 2 ){
+        return 1;
+    }
+    let n1 = 1;
+    let n2 = 1;
+    let sum = 0;
 
+    for(let i = 2; i < index; i++){
+        sum = n1 + n2;
+        n1 = n2;
+        n2 = sum;
+
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+//1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
+
+
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+    return obj.reduce( (old, current) => {
+
+    const older = getAge(old.yearOfBirth, old.yearOfDeath);
+    const currenter = getAge(current.yearOfBirth, current.yearOfDeath);
+    
+    if(older>currenter){
+        return old;
+    } else {
+       return current;
+    }
+
+        
+
+    });
+    
 
 };
+
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+};
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
